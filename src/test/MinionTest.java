83f0c6fad4bd996321890c441f8ffe8c9fb916c3
HonestBook <lsmwallfacer@gmail.com>
2021-06-01 11:17:56 +0800

    Create first test

---
+++ src/test/MinionTest.java
@@ -0,0 +1,39 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class MinionTest {
+
+    @Test
+    void testConstructor() {
+        Minion minion = new Minion("23Dragon", 2, 3, 1);
+        assertEquals("23Dragon", minion.getName());
+        assertEquals(2, minion.getAttack());
+        assertEquals(3, minion.getHealth());
+        assertEquals(1, minion.getTier());
+    }
+
+    @Test
+    void getName() {
+    }
+
+    @Test
+    void getAttack() {
+    }
+
+    @Test
+    void getHealth() {
+    }
+
+    @Test
+    void getTier() {
+    }
+
+    @Test
+    void loseHP() {
+    }
+
+    @Test
+    void isAlive() {
+    }
+}
