06daeb2b0e43eb2549250c59b40e034c89fa0bec
HonestBook <lsmwallfacer@gmail.com>
2021-06-18 01:42:59 +0800

    Fix gradle

--- src/main/java/gui/MainApplication.java
+++ src/main/java/gui/MainApplication.java
@@ -1,34 +1,35 @@
 package gui;
 
 import javafx.application.Application;
-import javafx.event.Event;
-import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
+import java.io.IOException;
+
+
 public class MainApplication extends Application {
+    private final static int MIN_HEIGHT = 800;
+    private final static int MIN_WIDTH = 1200;
+
     public static void main(String[] args) {
         launch(args);
     }
 
     @Override
-    public void start(Stage primaryStage) {
-        primaryStage.setTitle("Hello World!");
-        Button btn = new Button();
-        btn.setText("Say 'Hello World'");
-        btn.setOnAction(new EventHandler() {
+    public void start(Stage primaryStage) throws IOException {
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource("../../resources/main-window.fxml"));
+            primaryStage.setTitle("Hearthstone Battlegrounds Simulator");
+            primaryStage.setScene(new Scene(root));
+        } catch (Exception e) {
+            System.out.println("FXMLLoader failed");
+            e.printStackTrace();
+        }
 
-            @Override
-            public void handle(Event event) {
-                System.out.println("Hello World!");
-            }
-        });
-
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);
-        primaryStage.setScene(new Scene(root, 300, 250));
+        primaryStage.setMinHeight(MIN_HEIGHT);
+        primaryStage.setMinWidth(MIN_WIDTH);
         primaryStage.show();
     }
 }
--- src/test/java/MinionTest.java
+++ src/test/java/MinionTest.java
@@ -1,4 +1,4 @@
-import java.model.Minion;
+import model.Minion;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
