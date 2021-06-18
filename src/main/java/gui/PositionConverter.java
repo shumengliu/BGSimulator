b71a536675973ac4fec6a57bede8ec003fdc8757
HonestBook <lsmwallfacer@gmail.com>
2021-06-18 20:49:20 +0800

    Add simple createMinion function

---
+++ src/main/java/gui/PositionConverter.java
@@ -0,0 +1,11 @@
+package gui;
+
+import com.google.common.collect.BiMap;
+import com.google.common.collect.ImmutableMap;
+import javafx.scene.control.Label;
+import model.Position;
+
+public class PositionConverter {
+//    private static final BiMap<Position, Label> positionToLabelsBiMap = new ImmutableMap.Builder<Position, Label>()
+//            .put(Position.A1, )
+}
