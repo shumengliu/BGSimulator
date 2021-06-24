a7487d2514c12e4d91df8dd6b3397a72a31e0495
HonestBook <lsmwallfacer@gmail.com>
2021-06-24 17:04:27 +0800

    Refactor createMinion to MinionPane

--- src/main/java/gui/MainWindowController.java
+++ src/main/java/gui/MainWindowController.java
@@ -1,78 +1,13 @@
 package gui;
 
-import com.google.common.collect.ImmutableMap;
 import gui.components.MinionPane;
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import model.Minion;
 import model.Position;
 import model.Simulator;
 
-import java.util.Map;
 
 public class MainWindowController {
     private Simulator simulator;
-
-    private Map<MinionPane, Position> panePositionMap;
-    @FXML
-    public Label minionNameA1;
-    @FXML
-    public Label minionNameA2;
-    @FXML
-    public Label minionNameA3;
-    @FXML
-    public Label minionNameA4;
-    @FXML
-    public Label minionNameA5;
-    @FXML
-    public Label minionNameA6;
-    @FXML
-    public Label minionNameA7;
-    @FXML
-    public Label minionNameB1;
-    @FXML
-    public Label minionNameB2;
-    @FXML
-    public Label minionNameB3;
-    @FXML
-    public Label minionNameB4;
-    @FXML
-    public Label minionNameB5;
-    @FXML
-    public Label minionNameB6;
-    @FXML
-    public Label minionNameB7;
-
-    @FXML
-    public Button createMinionButtonA1;
-    @FXML
-    public Button createMinionButtonA2;
-    @FXML
-    public Button createMinionButtonA3;
-    @FXML
-    public Button createMinionButtonA4;
-    @FXML
-    public Button createMinionButtonA5;
-    @FXML
-    public Button createMinionButtonA6;
-    @FXML
-    public Button createMinionButtonA7;
-    @FXML
-    public Button createMinionButtonB1;
-    @FXML
-    public Button createMinionButtonB2;
-    @FXML
-    public Button createMinionButtonB3;
-    @FXML
-    public Button createMinionButtonB4;
-    @FXML
-    public Button createMinionButtonB5;
-    @FXML
-    public Button createMinionButtonB6;
-    @FXML
-    public Button createMinionButtonB7;
 
     @FXML
     public MinionPane minionPaneA1;
@@ -106,42 +41,37 @@ public class MainWindowController {
     @FXML
     public void initialize() {
         simulator = new Simulator();
-        initializeBorderPanePositionMap();
+        initializeMinionPanes();
     }
 
-    private void initializeBorderPanePositionMap() {
-        panePositionMap = new ImmutableMap.Builder<MinionPane, Position>()
-                .put(minionPaneA1, Position.A1)
-                .put(minionPaneA2, Position.A2)
-                .put(minionPaneA3, Position.A3)
-                .put(minionPaneA4, Position.A4)
-                .put(minionPaneA5, Position.A5)
-                .put(minionPaneA6, Position.A6)
-                .put(minionPaneA7, Position.A7)
-                .put(minionPaneB1, Position.B1)
-                .put(minionPaneB2, Position.B2)
-                .put(minionPaneB3, Position.B3)
-                .put(minionPaneB4, Position.B4)
-                .put(minionPaneB5, Position.B5)
-                .put(minionPaneB6, Position.B6)
-                .put(minionPaneB7, Position.B7)
-                .build();
+    private void initializeMinionPanes() {
+        minionPaneA1.setSimulator(simulator);
+        minionPaneA2.setSimulator(simulator);
+        minionPaneA3.setSimulator(simulator);
+        minionPaneA4.setSimulator(simulator);
+        minionPaneA5.setSimulator(simulator);
+        minionPaneA6.setSimulator(simulator);
+        minionPaneA7.setSimulator(simulator);
+        minionPaneB1.setSimulator(simulator);
+        minionPaneB2.setSimulator(simulator);
+        minionPaneB3.setSimulator(simulator);
+        minionPaneB4.setSimulator(simulator);
+        minionPaneB5.setSimulator(simulator);
+        minionPaneB6.setSimulator(simulator);
+        minionPaneB7.setSimulator(simulator);
+        minionPaneA1.setPosition(Position.A1);
+        minionPaneA2.setPosition(Position.A2);
+        minionPaneA3.setPosition(Position.A3);
+        minionPaneA4.setPosition(Position.A4);
+        minionPaneA5.setPosition(Position.A5);
+        minionPaneA6.setPosition(Position.A6);
+        minionPaneA7.setPosition(Position.A7);
+        minionPaneB1.setPosition(Position.B1);
+        minionPaneB2.setPosition(Position.B2);
+        minionPaneB3.setPosition(Position.B3);
+        minionPaneB4.setPosition(Position.B4);
+        minionPaneB5.setPosition(Position.B5);
+        minionPaneB6.setPosition(Position.B6);
+        minionPaneB7.setPosition(Position.B7);
     }
-
-    @FXML
-    public void createMinion(ActionEvent event) {
-        Button button = (Button) event.getSource();
-        MinionPane minionPane = (MinionPane) button.getParent();
-        Position position = getPositionByMinionPane(minionPane);
-        simulator.addMinionToBoard(new Minion(), position);
-    }
-
-    private Position getPositionByMinionPane(MinionPane pane) {
-        return panePositionMap.get(pane);
-    }
-//    private BorderPane getParentBorderPaneFromCreationButton(Button button) {
-//        BorderPane borderPane = (BorderPane) button.getParent();
-//        Label minionName = (Label) borderPane.getCenter();
-//        minionName.setText("Here is a minion.");
-//    }
 }
--- src/main/java/gui/components/MinionPane.java
+++ src/main/java/gui/components/MinionPane.java
@@ -1,8 +1,50 @@
 package gui.components;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
+import model.Minion;
+import model.Position;
+import model.Simulator;
+
+import java.io.IOException;
 
 public class MinionPane extends BorderPane {
+    private Simulator simulator;
+    private Position position;
+    @FXML
+    private TextField atkField;
+    @FXML
+    private TextField healthField;
+
+    public MinionPane() {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/minion-pane.fxml"));
+        fxmlLoader.setRoot(this);
+        fxmlLoader.setController(this);
+
+        try {
+            fxmlLoader.load();
+        } catch (IOException exception) {
+            throw new RuntimeException(exception);
+        }
+
+        atkField.setText("10");
+    }
+
+    @FXML
+    public void createMinion(ActionEvent event) {
+        simulator.addMinionToBoard(new Minion(), position);
+        simulator.simulateOnce();
+    }
 
 
+    public void setSimulator(Simulator simulator) {
+        this.simulator = simulator;
+    }
+
+    public void setPosition(Position position) {
+        this.position = position;
+    }
 }
