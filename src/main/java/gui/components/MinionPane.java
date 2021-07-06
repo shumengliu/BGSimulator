a1af1bb84fd0a9f1000c51eb9c8fed31b4198b28
HonestBook <lsmwallfacer@gmail.com>
2021-07-06 22:34:10 +0800

    Complete MinionPane

--- src/main/java/gui/components/MinionPane.java
+++ src/main/java/gui/components/MinionPane.java
@@ -4,6 +4,7 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.control.TextField;
+import javafx.scene.control.TextFormatter;
 import javafx.scene.layout.BorderPane;
 import model.Minion;
 import model.Position;
@@ -14,6 +15,7 @@ import java.io.IOException;
 public class MinionPane extends BorderPane {
     private Simulator simulator;
     private Position position;
+    private Minion minion;
     @FXML
     private TextField atkField;
     @FXML
@@ -30,15 +32,64 @@ public class MinionPane extends BorderPane {
             throw new RuntimeException(exception);
         }
 
-        atkField.setText("10");
+        addEventListeners();
     }
+
+    private void addEventListeners() {
+        setTextFormatters();
+        setModifyAttackListener();
+        setModifyHealthListener();
+    }
+
+    private void setTextFormatters() {
+        setTextFormatter(atkField);
+        setTextFormatter(healthField);
+    }
+
+    private void setTextFormatter(TextField field) {
+        field.setTextFormatter(new TextFormatter<>(c -> {
+            if (c.isContentChange()) {
+                if (c.getControlNewText().length() == 0) {
+                    return c;
+                }
+                try {
+                    Integer.parseInt(c.getControlNewText());
+                    return c;
+                } catch (NumberFormatException e) {
+                    return null;
+                }
+            }
+            return c;
+        }));
+    }
+
+    private void setModifyAttackListener() {
+        atkField.textProperty().addListener((observable, oldValue, newValue) -> {
+            int newAttack = Integer.parseInt(newValue);
+            minion.setAttack(newAttack);
+            updateMinionInBoard();
+        });
+    }
+
+    private void setModifyHealthListener() {
+        healthField.textProperty().addListener((observable, oldValue, newValue) -> {
+            int newHP = Integer.parseInt(newValue);
+            minion.setHP(newHP);
+            updateMinionInBoard();
+        });
+    }
+
 
     @FXML
     public void createMinion(ActionEvent event) {
-        simulator.addMinionToBoard(new Minion(), position);
-        simulator.simulateOnce();
+        minion = new Minion();
+        updateMinionInBoard();
     }
 
+    private void updateMinionInBoard() {
+        simulator.addMinionToBoard(minion, position);
+        simulator.simulateOnce(); // todo delete this when test is completed
+    }
 
     public void setSimulator(Simulator simulator) {
         this.simulator = simulator;
